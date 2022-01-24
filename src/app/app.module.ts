9e6c11a3ede99532173391d6a1ef60e18721b76a
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2022-01-24 01:20:54 -0800

    feat: add Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GamerComponent } from './gamer/gamer.component';
 import { GameComponent } from './gamer/game/game.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { GameComponent } from './gamer/game/game.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
